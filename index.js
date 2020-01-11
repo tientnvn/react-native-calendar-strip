f99d9aad9b31e7b5adf4713509f9e70c7be60f36
Tien Nguyen <toantien.vn91@gmail.com>
2020-01-11 18:34:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
  * Created by bogdanbegovic on 8/29/16.
  */
 const Calendar = require("./src/CalendarStrip");
-const CalendarDay = require("./src/CalendarDay");
 
-module.exports = {Calendar, CalendarDay};
+module.exports = Calendar;
