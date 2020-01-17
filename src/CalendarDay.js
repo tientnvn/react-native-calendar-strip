0f270d2de0de4ef9f3b2189560dd178647cf25cb
Tien Nguyen <toantien.vn91@gmail.com>
2020-01-17 22:27:08 +0700

    Update CalendarDay.js

--- src/CalendarDay.js
+++ src/CalendarDay.js
@@ -243,7 +243,7 @@ class CalendarDay extends Component {
               style={[{ fontSize: this.state.dateNameFontSize }, dateNameStyle]}
               allowFontScaling={this.props.allowDayTextScaling}
             >
-              {this.props.date.isSame(new Date(), "days") ? "Hôm nay": this.props.date.format("dddd")}
+              {this.props.date.isSame(new Date(), "days") ? "Hôm nay": this.props.date.format("ddd")}
             </Text>
           )}
           {this.props.showDayNumber && (
